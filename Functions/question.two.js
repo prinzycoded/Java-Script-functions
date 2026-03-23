a39a0970ffc92c310d0bdc55d7635c748e1eff4f
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-23 12:53:38 +0100

    question two

---
+++ Functions/question.two.js
@@ -0,0 +1,9 @@
+function iseven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(iseven(4)); // true
+console.log(iseven(7)); // false
