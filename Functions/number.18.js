9a3856c961617a5f5cdd2978a09d33d5bc91a817
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-24 17:58:55 +0100

    number 18

---
+++ Functions/number.18.js
@@ -0,0 +1,14 @@
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log("Fizz");
+        } else if(i % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+fizzBuzz(15); // 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"
