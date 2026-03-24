90f9cccfa1b02d92223a7af7b8dd8df7c5cd5e9a
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-24 17:49:59 +0100

    number 17

---
+++ Functions/number.17.js
@@ -0,0 +1,6 @@
+function wordCount(str) {
+    return str.split(" ").length;
+}
+console.log(wordCount("I Love Programming")); // 3
+console.log(wordCount("Prinzy Tech")); // 2
+console.log(wordCount("JavaScript is fun")); // 3
